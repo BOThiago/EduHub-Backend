415401c6e131916987ccf38ff466ef382e8f023d
BOThiago <thiago.barreto.oliveira22@gmail.com>
2024-08-12 23:51:06 -0300

    feat: course banner

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,6 +1,7 @@
 class Course < ApplicationRecord
   has_many :course_files, dependent: :destroy
   accepts_nested_attributes_for :course_files
+  has_one_attached :course_banner
 
   def total_size_in_mb
     total_size_in_bytes = course_files.sum do |course_file|
@@ -11,5 +12,9 @@ class Course < ApplicationRecord
       end
     end
     (total_size_in_bytes.to_f / 1.megabyte).round(2)
+  end
+
+  def course_banner_url
+    Rails.application.routes.url_helpers.rails_blob_url(course_banner, only_path: true) if course_banner.attached?
   end
 end
---
+++ db/migrate/20240813021445_add_course_banner_to_courses.rb
@@ -0,0 +1,4 @@
+class AddCourseBannerToCourses < ActiveRecord::Migration[7.1]
+  def change
+  end
+end
